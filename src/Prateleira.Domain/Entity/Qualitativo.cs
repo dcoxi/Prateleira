c43eb60b5c1761689fb1f86bbabbeee348622992
Dilangue Coxi <dilangue.coxi@tistech.co.ao>
2022-02-23 17:11:44 +0100

    main: remorção de id desnecessário

--- src/Prateleira.Domain/Entity/Qualitativo.cs
+++ src/Prateleira.Domain/Entity/Qualitativo.cs
@@ -5,7 +5,6 @@ namespace Prateleira.Domain.Entity
 {
     public class Qualitativo
     {
-        public Guid Id { get; set; }
         public UnidadeMedida UnidadeMedida { get; set; }
         public decimal PrecoUnidade { get; set; } 
         public int Quantidade { get; set; }
